8485facebb06e4129a5b7969c8c8e797ec528b0d
sistemas <sistemas@pop-os.localdomain>
2024-03-01 11:08:10 -0600

    Completed Challenge 1

--- Challenge 1/vmconfig.sh
+++ Challenge 1/vmconfig.sh
@@ -13,7 +13,7 @@ do
     d) disk=${OPTARG};;
     s) sataName=${OPTARG};;
     i) ideName=${OPTARG};;
-    h) echo -e "SYNTAX:\n\tvmconfig.sh [OPTIONS] [STRING]\n\nOPTIONS:\n\t-n: Nombre de la máquina virtual.\n\t-o: Tipo de sistema operativo.\n\t-c: Número de CPUs.\n\t-r: Cantidad de RAM en GB.\n\t-v: Cantidad de vRAM en MB.\n\t-d: Tamaño del disco virtual.\n\t-s: Nombre del controlador SATA.\n\t-i: Nombre del controlador IDE.\n" 
+    h) echo -e "SYNTAX:\n\tvmconfig.sh [OPTIONS] [STRING]\n\nOPTIONS:\n\t-n: Nombre de la máquina virtual.\n\t-o: Tipo de sistema operativo.\n\t-c: Número de CPUs.\n\t-r: Cantidad de RAM en MB.\n\t-v: Cantidad de vRAM en MB.\n\t-d: Tamaño del disco virtual en MB.\n\t-s: Nombre del controlador SATA.\n\t-i: Nombre del controlador IDE.\n" 
       exit ;;
   esac
 done
@@ -23,8 +23,22 @@ if [[ -z "$name" || -z "$os" || -z "$cpu" || -z "$ram" || -z "$vram" || -z "$dis
   exit 
 fi
 
-mkdir $name
-cd name
-VBoxManage createvm --name $name --ostype $os --register --basefolder 'pwd'
+VBoxManage createvm --name $name --ostype $os --register --basefolder 'pwd'/$name
 VBoxManage modifyvm $name --ioapic on 
+VBoxManage modifyvm $name --cpus $cpu
 VBoxManage modifyvm $name --memory $ram --vram $vram
+VBoxManage createmedium --filename 'pwd'/$name/$name.vhd --size $disk --format VHD
+VBoxManage storagectl $name --name $sataName --add sata --controller IntelAhci
+VBoxManage storageattach $name --storagectl $sataName --port 0 --device 0 --type hdd --medium 'pwd'/$name/$name.vhd
+VBoxManage storagectl $name --name $ideName --add ide 
+VBoxManage storageattach $name --storagectl $ideName --port 1 --device 0 --type dvddrive --medium emptydrive
+
+VBoxManage showvminfo $name | grep "Name:"
+VBoxManage showvminfo $name | grep "Guest OS:"
+VBoxManage showvminfo $name | grep "Number of CPUs:"
+VBoxManage showvminfo $name | grep "Memory size:"
+VBoxManage showvminfo $name | grep "VRAM size:"
+VBoxManage showvminfo $name --machinereadable | grep "storagecontrollername0=" 
+VBoxManage showvminfo $name --machinereadable | grep "storagecontrollername1=" 
+VBoxManage showvminfo $name --machinereadable | grep "SATAController-0-0" 
+VBoxManage showvminfo $name --machinereadable | grep "IDEController-1-0" 
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Sem-SistemasOperativos
 https://andreafortuna.org/2019/10/24/how-to-create-a-virtualbox-vm-from-command-line/
 https://superuser.com/questions/815057/virtualbox-increase-cpu-cores-script-wise
+https://www.oracle.com/mx/technical-resources/articles/it-infrastructure/admin-manage-vbox-cli.html
